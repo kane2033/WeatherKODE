0da4534ff45eb41e8bbb8fc370c5a85aab13a1b4
kane2033 <u-cent@yandex.ru>
2021-05-03 21:31:05 +0700

    Add animation to city info view.

--- app/src/main/java/com/kode/weather/presentation/base/BindingAdapters.kt
+++ app/src/main/java/com/kode/weather/presentation/base/BindingAdapters.kt
@@ -1,11 +1,16 @@
 package com.kode.weather.presentation.base
 
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.widget.ContentLoadingProgressBar
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
+import androidx.transition.Slide
+import androidx.transition.Transition
+import androidx.transition.TransitionManager
 import com.squareup.picasso.Picasso
+
 
 object BindingAdapters {
 
@@ -23,5 +28,14 @@ object BindingAdapters {
     @BindingAdapter(value = ["loading"])
     fun ContentLoadingProgressBar.setLoading(isLoading: Boolean) {
         if (isLoading) this.show() else this.hide()
+    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["visibilityAnimated"])
+    fun View.animateOnVisibilityChanged(visibility: Int) {
+        val transition: Transition = Slide(Gravity.BOTTOM)
+        transition.addTarget(this)
+        TransitionManager.beginDelayedTransition(parent as ViewGroup, transition)
+        this.visibility = visibility
     }
 }
