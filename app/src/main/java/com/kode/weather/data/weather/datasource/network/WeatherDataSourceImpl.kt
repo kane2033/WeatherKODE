28568c4dd015d71cce8e8f95fbb9d1cb3edf26e5
kane2033 <u-cent@yandex.ru>
2021-05-19 19:05:35 +0700

    Fix Code Review 2 [#3] NetworkModule#provideOkHttpClient.
    WeatherModule.kt now created a different instance of Retrofit with OkHttpClient that has auth interceptor.

--- app/src/main/java/com/kode/weather/data/weather/datasource/network/WeatherDataSourceImpl.kt
+++ app/src/main/java/com/kode/weather/data/weather/datasource/network/WeatherDataSourceImpl.kt
@@ -3,15 +3,24 @@ package com.kode.weather.data.weather.datasource.network
 import com.kode.weather.data.base.network.extention.getBodySafely
 import com.kode.weather.data.weather.datasource.network.api.OpenWeatherApi
 import com.kode.weather.data.weather.datasource.network.converter.toWeather
+import com.kode.weather.di.weather.WeatherModule
 import com.kode.weather.domain.base.exception.Failure
 import com.kode.weather.domain.weather.datasource.WeatherDataSource
 import com.kode.weather.domain.weather.entity.Weather
 import com.kode.weather.domain.weather.entity.WeatherQuery
 import com.kode.weather.domain.weather.exception.FetchWeatherFailure
+import org.koin.core.qualifier.named
+import org.koin.java.KoinJavaComponent.inject
+
 import retrofit2.Retrofit
 import java.net.HttpURLConnection
 
-class WeatherDataSourceImpl(retrofit: Retrofit) : WeatherDataSource {
+class WeatherDataSourceImpl() : WeatherDataSource {
+
+    private val retrofit: Retrofit by inject(
+        clazz = Retrofit::class.java,
+        qualifier = named(WeatherModule.RETROFIT_AUTH)
+    )
 
     private val api: OpenWeatherApi by lazy { retrofit.create(OpenWeatherApi::class.java) }
 
--- app/src/main/java/com/kode/weather/di/weather/WeatherModule.kt
+++ app/src/main/java/com/kode/weather/di/weather/WeatherModule.kt
@@ -1,17 +1,34 @@
 package com.kode.weather.di.weather
 
+import com.kode.weather.data.base.network.NetworkAvailabilityInterceptor
 import com.kode.weather.data.weather.datasource.network.WeatherDataSourceImpl
+import com.kode.weather.data.weather.datasource.network.interceptor.OpenWeatherAuthInterceptor
+import com.kode.weather.di.base.NetworkModule
 import com.kode.weather.domain.weather.datasource.WeatherDataSource
 import com.kode.weather.domain.weather.usecase.FetchCityWeather
 import com.kode.weather.presentation.weather.WeatherViewModel
+import okhttp3.OkHttpClient
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 object WeatherModule {
+
+    const val RETROFIT_AUTH = "auth"
+    private const val CLIENT_AUTH = "client_auth"
+
     val module = module {
-        single<WeatherDataSource> { WeatherDataSourceImpl(get()) }
+        single(named(CLIENT_AUTH)) { provideOkHttpClientWithAuth(get()) }
+        single(named(RETROFIT_AUTH)) { NetworkModule.provideRetrofit(get(named(CLIENT_AUTH))) }
+
+        single<WeatherDataSource> { WeatherDataSourceImpl() }
         single { FetchCityWeather(get()) }
 
         viewModel { WeatherViewModel(get(), get()) }
+    }
+
+    private fun provideOkHttpClientWithAuth(interceptor: NetworkAvailabilityInterceptor): OkHttpClient {
+        val builder = NetworkModule.provideOkHttpClient(interceptor).newBuilder()
+        return builder.addInterceptor(OpenWeatherAuthInterceptor()).build()
     }
 }
